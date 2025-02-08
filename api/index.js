f106f9d1f742ec7ed54a168bcd160e7acbc5cd22
Razzak05 <razzakchoudhary9619@gmail.com>
2025-02-08 09:45:37 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 connectDB();
 
@@ -9,3 +10,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
